03554441a3cea0d778e6d71b06b667079f6f8ca8
Ramona Łuczkiewicz <ramona@luczkiewi.cz>
2026-01-25 19:15:20 +0100

    store the corpus for fuzzing in git

--- xdb/src/lib.rs
+++ xdb/src/lib.rs
@@ -1,4 +1,3 @@
-// TODO store the fuzz corpuses somewhere (git or not git? idk)
 // TODO write a script which will run all the tests (+miri) and all the fuzz targets
 // TODO create a server + a test app which will run many threads with many simulatenous
 // transactions to strees test
